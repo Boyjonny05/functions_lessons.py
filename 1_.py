9658723f3ce44732456bf06f118c9cde0f86bb59
Boyjonny05 <Void59315@gmail.com>
2026-01-27 14:35:16 +0000

    Work

--- 1_.py
+++ 1_.py
@@ -1,4 +1,46 @@
-# Methods, Help & Documentation Practice #1
+# function = a block of reusable code
+# Place () after the function name to invoke it
+
+# def happy_birthday(name , age):
+#     print(f"happy birthday to {name}!")
+#     print("you are {age}!")
+#     print("Happy birthday to you!")
+#     print()
+
+# happy_birthday("Bro", 20)
+# happy_birthday("Steve", 30)
+# happy_birthday("Joe", 30)
+
+# def display_invoice(username, amount, due_date):
+#     print(f"Hello {username}")
+#     print(f"Your bill of ${amount:,2f} is due: {due_date}")
+
+# display_invoice("BroCode", 42.50, "01/01")
+
+# def add(x, y):
+#     z = x + y
+#     return z
+# def subtract(x, y):
+#     z = x - y
+#     return z
+# def Multiply(x, y):
+#     z = x * y
+#     return z
+# def Divide(x, y):
+#     z = x / y
+#     return z
+
+# print(add(1, 2))
+# print(subtract(1, 2))
+# print(Multiply(1, 2))
+# print(Divide(1, 2))
+
+def create_name(first,last):
+    first = first.capitalize()
+    last = last.capitalize()
+    full_name = f"{first} {last}"
+    return full_name
+# Methods, Help & Documentation Practice #
 # Remove the characters to the left of our main text:
 
 # ,
